7a422bc21da8ad1d116e6cff100735250d622cdc
Greg Turnquist <gturnquist@gopivotal.com>
2013-08-27 10:32:19 -0500

    Remove import for EnableWebMvc when the annotation was struck

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
