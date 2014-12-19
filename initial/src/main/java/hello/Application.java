971a8e67cbd2bccbb318aa5ec26a0808fd9c06b6
Greg Turnquist <gturnquist@gopivotal.com>
2014-12-19 09:02:39 -0600

    Polish up language on MultipartConfigElement
    * Fixes #10

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,24 +1,12 @@
 package hello;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.MultipartConfigElement;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-
-    @Bean
-    MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        factory.setMaxFileSize("128KB");
-        factory.setMaxRequestSize("128KB");
-        return factory.createMultipartConfig();
-    }
 
 }
