b1c220a36388b8950d97fe0c7f7c77790e030e7c
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 10:37:19 -0600

    Upgrade to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,13 +1,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
