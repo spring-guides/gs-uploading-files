e472437007239d271ea47dedf0f6dc5930b57dfa
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 10:43:09 -0600

    Convert initial version to @SpringBootApplication

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,12 +1,8 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
 }
