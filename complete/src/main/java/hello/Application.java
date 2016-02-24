c536c798df41a716ea17444ae99923b75a08ca61
Greg Turnquist <gturnquist@pivotal.io>
2016-02-24 12:06:19 -0600

    Enhance experience
    * SOup up with a Thymeleaf template including a conditional flash message
    * Show existing files
    * Use FileCopyUtils and Multipart.getInputStream()
    
    Resolves #19, #9

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,25 @@
 package hello;
 
+import java.io.File;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+    public static String ROOT = "upload-dir";
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init() {
+        return (String[] args) -> {
+            new File(ROOT).mkdir();
+        };
     }
 }
--- complete/src/main/java/hello/FileUploadController.java
+++ complete/src/main/java/hello/FileUploadController.java
@@ -3,39 +3,73 @@ package hello;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FileUploadController {
-    
-    @RequestMapping(value="/upload", method=RequestMethod.GET)
-    public @ResponseBody String provideUploadInfo() {
-        return "You can upload a file by posting to this same URL.";
-    }
-    
-    @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("name") String name, 
-            @RequestParam("file") MultipartFile file){
-        if (!file.isEmpty()) {
-            try {
-                byte[] bytes = file.getBytes();
-                BufferedOutputStream stream = 
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-                stream.write(bytes);
-                stream.close();
-                return "You successfully uploaded " + name + "!";
-            } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
-            }
-        } else {
-            return "You failed to upload " + name + " because the file was empty.";
-        }
-    }
+
+	@RequestMapping(method = RequestMethod.GET, value = "/upload")
+	public String provideUploadInfo(Model model) {
+		File rootFolder = new File(Application.ROOT);
+		List<String> fileNames = Arrays.stream(rootFolder.listFiles())
+			.map(f -> f.getName())
+			.collect(Collectors.toList());
+
+		model.addAttribute("files",
+			Arrays.stream(rootFolder.listFiles())
+					.sorted(Comparator.comparingLong(f -> -1 * f.lastModified()))
+					.map(f -> f.getName())
+					.collect(Collectors.toList())
+		);
+
+		return "uploadForm";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/upload")
+	public String handleFileUpload(@RequestParam("name") String name,
+								   @RequestParam("file") MultipartFile file,
+								   RedirectAttributes redirectAttributes) {
+		if (name.contains("/")) {
+			redirectAttributes.addFlashAttribute("message", "Folder separators not allowed");
+			return "redirect:upload";
+		}
+		if (name.contains("/")) {
+			redirectAttributes.addFlashAttribute("message", "Relative pathnames not allowed");
+			return "redirect:upload";
+		}
+
+		if (!file.isEmpty()) {
+			try {
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File(Application.ROOT + "/" + name)));
+                FileCopyUtils.copy(file.getInputStream(), stream);
+				stream.close();
+				redirectAttributes.addFlashAttribute("message",
+						"You successfully uploaded " + name + "!");
+			}
+			catch (Exception e) {
+				redirectAttributes.addFlashAttribute("message",
+						"You failed to upload " + name + " => " + e.getMessage());
+			}
+		}
+		else {
+			redirectAttributes.addFlashAttribute("message",
+					"You failed to upload " + name + " because the file was empty");
+		}
+
+		return "redirect:upload";
+	}
 
 }
