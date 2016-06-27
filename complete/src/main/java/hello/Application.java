bc402eea88422278b51e3a4735be5ac371eab286
Greg Turnquist <gturnquist@pivotal.io>
2016-06-27 14:38:02 -0500

    Update the guide using new APIS
    * Use Java NIO for file operations (avoids "/" vs "\" among other things)
    * Use Java 8 streams to find all the files and transform them into a list of links for each file
    
    Resolves #24, #22, #7, #9

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,25 +1,30 @@
 package hello;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.FileSystemUtils;
 
 @SpringBootApplication
 public class Application {
 
-    public static String ROOT = "upload-dir";
+	public static String ROOT = "upload-dir";
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Bean
-    CommandLineRunner init() {
-        return (String[] args) -> {
-            new File(ROOT).mkdir();
-        };
-    }
+	@Bean
+	CommandLineRunner init() {
+		return (args) -> {
+            FileSystemUtils.deleteRecursively(new File(ROOT));
+
+            Files.createDirectory(Paths.get(ROOT));
+		};
+	}
 }
--- complete/src/main/java/hello/FileUploadController.java
+++ complete/src/main/java/hello/FileUploadController.java
@@ -1,72 +1,78 @@
 package hello;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FileUploadController {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/")
-	public String provideUploadInfo(Model model) {
-		File rootFolder = new File(Application.ROOT);
-		List<String> fileNames = Arrays.stream(rootFolder.listFiles())
-			.map(f -> f.getName())
-			.collect(Collectors.toList());
+	private static final Logger log = LoggerFactory.getLogger(FileUploadController.class);
 
-		model.addAttribute("files",
-			Arrays.stream(rootFolder.listFiles())
-					.sorted(Comparator.comparingLong(f -> -1 * f.lastModified()))
-					.map(f -> f.getName())
-					.collect(Collectors.toList())
-		);
+	private final ResourceLoader resourceLoader;
+
+	@Autowired
+	public FileUploadController(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String provideUploadInfo(Model model) throws IOException {
+
+		model.addAttribute("files", Files.walk(Paths.get(Application.ROOT))
+				.filter(path -> !path.equals(Paths.get(Application.ROOT)))
+				.map(path -> Paths.get(Application.ROOT).relativize(path))
+				.map(path -> linkTo(methodOn(FileUploadController.class).getFile(path.toString())).withRel(path.toString()))
+				.collect(Collectors.toList()));
 
 		return "uploadForm";
 	}
 
+	@RequestMapping(method = RequestMethod.GET, value = "/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<?> getFile(@PathVariable String filename) {
+
+		try {
+			return ResponseEntity.ok(resourceLoader.getResource("file:" + Paths.get(Application.ROOT, filename).toString()));
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
 	@RequestMapping(method = RequestMethod.POST, value = "/")
-	public String handleFileUpload(@RequestParam("name") String name,
-								   @RequestParam("file") MultipartFile file,
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
 								   RedirectAttributes redirectAttributes) {
-		if (name.contains("/")) {
-			redirectAttributes.addFlashAttribute("message", "Folder separators not allowed");
-			return "redirect:/";
-		}
-		if (name.contains("/")) {
-			redirectAttributes.addFlashAttribute("message", "Relative pathnames not allowed");
-			return "redirect:/";
-		}
 
 		if (!file.isEmpty()) {
 			try {
-				BufferedOutputStream stream = new BufferedOutputStream(
-						new FileOutputStream(new File(Application.ROOT + "/" + name)));
-                FileCopyUtils.copy(file.getInputStream(), stream);
-				stream.close();
+				Files.copy(file.getInputStream(), Paths.get(Application.ROOT, file.getOriginalFilename()));
 				redirectAttributes.addFlashAttribute("message",
-						"You successfully uploaded " + name + "!");
+						"You successfully uploaded " + file.getOriginalFilename() + "!");
+			} catch (IOException|RuntimeException e) {
+				redirectAttributes.addFlashAttribute("message", "Failued to upload " + file.getOriginalFilename() + " => " + e.getMessage());
 			}
-			catch (Exception e) {
-				redirectAttributes.addFlashAttribute("message",
-						"You failed to upload " + name + " => " + e.getMessage());
-			}
-		}
-		else {
-			redirectAttributes.addFlashAttribute("message",
-					"You failed to upload " + name + " because the file was empty");
+		} else {
+			redirectAttributes.addFlashAttribute("message", "Failed to upload " + file.getOriginalFilename() + " because it was empty");
 		}
 
 		return "redirect:/";
