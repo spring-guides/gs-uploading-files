687aeb9df5f0899015fc2b49be2bcc1a4a48d16f
Dave Syer <dsyer@pivotal.io>
2017-07-28 09:28:29 +0100

    Whitespace (spaces instead of tabs)

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,26 +1,27 @@
 package hello;
 
-import hello.storage.StorageProperties;
-import hello.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
+
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
             storageService.deleteAll();
             storageService.init();
-		};
-	}
+        };
+    }
 }
