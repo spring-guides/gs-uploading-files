4b5479363d74873aed92058846352db3e6216fb7
AmrKhaledNowahy <41016562+AmrKhaledNowahy@users.noreply.github.com>
2021-02-10 11:42:01 +0200

    persist uploaded files on application booting and exiting

--- complete/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ complete/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -20,7 +20,7 @@ public class UploadingFilesApplication {
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
-			storageService.deleteAll();
+			//storageService.deleteAll();
 			storageService.init();
 		};
 	}
