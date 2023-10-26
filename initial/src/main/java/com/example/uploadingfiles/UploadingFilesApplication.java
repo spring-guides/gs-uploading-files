b4f0536cf72c7b787bb74f6d3168980377e2aefa
Aliaksandr Shtarou <aeresfiru@gmail.com>
2023-10-26 14:46:12 +0300

    Add @EnableConfigurationProperties for StorageProperties
    Added the @EnableConfigurationProperties(StorageProperties.class) annotation
    to the UploadingFilesApplication class in the "initial" project.

--- initial/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ initial/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -2,8 +2,12 @@ package com.example.uploadingfiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.uploadingfiles.storage.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class UploadingFilesApplication {
 
 	public static void main(String[] args) {
