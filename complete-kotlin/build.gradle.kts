9c0eda53864f1b0d2cd5e802f5e872d3136a3ceb
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-12-19 18:23:01 +0100

    Add missing dependency

--- complete-kotlin/build.gradle.kts
+++ complete-kotlin/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-restclient")
 }
 
 kotlin {
